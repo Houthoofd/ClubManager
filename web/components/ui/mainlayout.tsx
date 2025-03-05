7dfa872fac620e736f4cd7a6bffa2990ae7831ad
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-03-05 15:29:34 +0100

    update ui

--- web/components/ui/mainlayout.tsx
+++ web/components/ui/mainlayout.tsx
@@ -3,24 +3,35 @@ import { Link } from 'react-router-dom';
 import NotificationBell from '../ui/notification-bell';
 import { RightExpandButton, LeftExpandButton } from '../ui/expanse-btn';
 import MessageIcon from '../ui/message-icon';
+import { useDispatch, useSelector } from 'react-redux';
+ 
 
 interface MainLayoutProps {
-  children?: ReactNode;  // Spécifie que children est attendu ici
+  children?: ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const dispatch = useDispatch();
+
+  // Utilisation des selectors pour récupérer les états de navigation
+  const isLeftNavbarOpen = useSelector((state: any) => state.navigation.left_navbar);
+  const isRightNavbarOpen = useSelector((state: any) => state.navigation.right_navbar);
+  const isRightHeaderPanelOpen = useSelector((state: any) => state.navigation.header_right_panel);
+
+  console.log({isLeftNavbarOpen: isLeftNavbarOpen, isRightNavbarOpen: isRightNavbarOpen, isRightHeaderPanelOpen: isRightHeaderPanelOpen})
+
   return (
-    <div className="layout">
-      <header>
+    <div className='layout'>
+      <header className={`header ${isLeftNavbarOpen ? 'left-navbar-open' : 'left-navbar-close'} ${isRightHeaderPanelOpen ? 'right-header-open' : 'right-header-close'}`}>
         <div className='layer'>
-          <LeftExpandButton />
+          <LeftExpandButton /> {/* Bascule du navbar gauche */}
         </div>
         <div className='header-content'>
           {/* Contenu supplémentaire pour le header ici */}
         </div>
         <nav className='header-right-navbar'>
           <ul>
-            <RightExpandButton />
+            <RightExpandButton /> {/* Bascule du navbar droit */}
             <NotificationBell />
             <Link to="/pages/messages">
               <MessageIcon />
@@ -29,20 +40,20 @@ const MainLayout = ({ children }: MainLayoutProps) => {
         </nav>
       </header>
 
-      <div className="content">
-        <nav className='navigation-left-panel'>
+      <div className={`content ${isLeftNavbarOpen ? 'left-navbar-open' : 'left-navbar-close'} ${isRightHeaderPanelOpen ? 'right-navbar-open' : 'right-navbar-close'}`}>
+        <nav className={`navigation-left-panel ${isLeftNavbarOpen ? 'open' : ''}`}>
           <ul>
             <li><Link to="/pages/cours">Cours</Link></li>
             <li><Link to="/pages/dashboard">Dashboard</Link></li>
           </ul>
         </nav>
 
-        {/* Ici on affiche les composants enfants */}
+        {/* Affichage des composants enfants */}
         <div className="main-content">
           {children}
         </div>
 
-        <div className="right-panel-content">
+        <div className={`right-panel-content ${isRightNavbarOpen ? 'open' : 'close'}`}>
           {/* Contenu supplémentaire pour le panneau droit ici */}
         </div>
       </div>
