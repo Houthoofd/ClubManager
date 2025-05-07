f9838165c4a57506fc0e92508b447e14bef03efa
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-05-07 15:39:03 +0200

    upate

--- web/app/pages/chat.tsx
+++ web/app/pages/chat.tsx
@@ -191,9 +191,30 @@ const Chat = () => {
             border: '1px solid #ddd',
           }}
         />
-        <button onClick={handleSendMessage} style={{ padding: '5px 15px' }}>
-          Envoyer
+        <button
+          onClick={handleSendMessage}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '40px',
+            height: '40px', // Pour que le bouton soit bien carré
+            border: 'none',
+            backgroundColor: '#d1e7dd',
+            cursor: 'pointer',
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#5f6368"
+          >
+            <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+          </svg>
         </button>
+
       </div>
     </div>
   );
