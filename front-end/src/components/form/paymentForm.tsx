3246c35fc26a9aaa4c5e215068d1d8ffe989c1fa
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-06-17 11:58:17 +0200

    update

--- front-end/src/components/form/paymentForm.tsx
+++ front-end/src/components/form/paymentForm.tsx
@@ -7,7 +7,6 @@ import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
-console.log("Stripe public key:", import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 
 
