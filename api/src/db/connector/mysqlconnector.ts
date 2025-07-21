95c23f758879a04fc519039a6deca81c206eeaec
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-07-21 08:47:21 +0200

    update connection db

--- api/src/db/connector/mysqlconnector.ts
+++ api/src/db/connector/mysqlconnector.ts
@@ -1,58 +1,58 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Charge les variables d'environnement
 
 export default class MysqlConnector {
   private connection: mysql.Connection;
 
   constructor() {
-      // Configuration de la connexion à la base de données MySQL
-      this.connection = mysql.createConnection({
-        host: 'localhost', // Remplacez par votre endpoint RDS
-        port: 3306, // Le port par défaut pour MySQL
-        user: 'root', // Remplacez par votre nom d'utilisateur RDS
-        password: '', // Remplacez par votre mot de passe RDS
-        database: 'clubmanager',
-      });
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
-      // Établir la connexion à la base de données
-      this.connection.connect((err) => {
-          if (err) {
-              console.error('Erreur de connexion à la base de données : ' + err.stack);
-              return;
-          }
-          console.log('Connecté à la base de données MySQL avec l\'ID : ' + this.connection.threadId);
-      });
-  }
-
-  // Méthode pour exécuter des requêtes SQL
-  public query(sql: string, values: any[] = [], callback: (error: mysql.MysqlError | null, results?: any, fields?: mysql.FieldInfo[]) => void): void {
-    this.connection.query(sql, values, (error, results, fields) => {
-        callback(error, results, fields);
+    this.connection.connect((err) => {
+      if (err) {
+        console.error('Erreur de connexion à la base de données : ' + err.stack);
+        return;
+      }
+      console.log('Connecté à la base de données MySQL avec l\'ID : ' + this.connection.threadId);
     });
   }
 
-  // Démarrer une transaction
+  public query(
+    sql: string,
+    values: any[] = [],
+    callback: (error: mysql.MysqlError | null, results?: any, fields?: mysql.FieldInfo[]) => void
+  ): void {
+    this.connection.query(sql, values, (error, results, fields) => {
+      callback(error, results, fields);
+    });
+  }
+
   public beginTransaction(callback: (err: mysql.MysqlError | null) => void): void {
     this.connection.beginTransaction(callback);
   }
 
-  // Commit une transaction
   public commit(callback: (err: mysql.MysqlError | null) => void): void {
     this.connection.commit(callback);
   }
 
-  // Rollback une transaction
   public rollback(callback: () => void): void {
     this.connection.rollback(callback);
   }
 
-  // Fermer la connexion
   public close(): void {
-      this.connection.end((err) => {
-          if (err) {
-              console.error('Erreur lors de la fermeture de la connexion : ' + err.stack);
-              return;
-          }
-          console.log('Connexion à la base de données MySQL fermée');
-      });
+    this.connection.end((err) => {
+      if (err) {
+        console.error('Erreur lors de la fermeture de la connexion : ' + err.stack);
+        return;
+      }
+      console.log('Connexion à la base de données MySQL fermée');
+    });
   }
 }
