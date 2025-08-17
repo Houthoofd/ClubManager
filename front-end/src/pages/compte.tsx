7f4d74fa9411a38314ca008df8378ef417fa2a24
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-08-17 12:34:47 +0200

    update

--- front-end/src/pages/compte.tsx
+++ front-end/src/pages/compte.tsx
@@ -54,7 +54,7 @@ const Compte = () => {
       }
       const result: VerifyResultWithData = await response.json();
       setCompte(result.data);
-      console.log(result.data);
+      console.log(result);
     } catch (error) {
       console.error('Erreur lors de la récupération des données:', error);
     }
