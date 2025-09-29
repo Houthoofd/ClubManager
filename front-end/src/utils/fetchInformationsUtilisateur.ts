a9c64f447edc15ff2ccb821ed4d7e88d59e45849
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2025-09-29 22:20:52 +0200

    update

--- front-end/src/utils/fetchInformationsUtilisateur.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchInformationsUtilisateur(prenom: string, nom: string): Promise<any> {
-  const response = await fetch('/api/compte/informations', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ prenom, nom }),
-  });
-  return await response.json();
-}
--- packages/types/dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from './utilisateurs.js';
-export * from './query.js';
-export * from './cours.js';
-export * from './magasin.js';
-export * from './statistiques.js';
